e0e45671c261150a26a5763b774f81336656776c
azykdev <azyk9114@gmail.com>
2024-02-19 20:09:54 +0500

    #2 - in uzbek lang

--- src/components/VideoStream.vue
+++ src/components/VideoStream.vue
@@ -2,7 +2,7 @@
   <div class="container">
       <div class="row">
           <div class="col-md-12 my-3">
-              <h2>Room</h2>
+              <h2>Xona</h2>
               <input v-model="roomId">
           </div>
       </div>
@@ -22,10 +22,10 @@
               </div>
               <div class="row">
                   <div class="col-md-12 my-3">
-                      <button type="button" class="btn btn-primary" @click="onJoin">Join</button>
-                      <button type="button" class="btn btn-primary" @click="onLeave">Leave</button>
-                      <button type="button" class="btn btn-primary" @click="onCapture">Capture Photo</button>
-                      <button type="button" class="btn btn-primary" @click="onShareScreen">Share Screen</button>
+                      <button type="button" class="btn btn-primary" @click="onJoin">Qo'shilish</button>
+                      <button type="button" class="btn btn-primary" @click="onLeave">Tark etish</button>
+                      <button type="button" class="btn btn-primary" @click="onCapture">Rasmga olish</button>
+                      <button type="button" class="btn btn-primary" @click="onShareScreen">Ekran ulashish</button>
                   </div>
               </div>
           </div>
